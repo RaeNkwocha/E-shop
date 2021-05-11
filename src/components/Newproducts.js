0410eb5b59d074e804347ce42391f3f615dc3678
unknown <raenkwoch@gmail.comm>
2021-05-11 08:15:58 +0100

    first commit

--- src/components/Newproducts.js
+++ src/components/Newproducts.js
@@ -4,13 +4,13 @@ import React from 'react';
 function Newproducts({name,image}) {
     return (
         <div className="new">
-            <div className="new-products">
+            <article className="new-products">
                 <div className="img-container">
                     <img src={image}></img>
                 </div>
                 <h5 className="explore">{name}</h5>
 
-            </div>
+            </article>
         </div>
     )
 }
